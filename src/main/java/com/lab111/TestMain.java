987cdfdda77331e216a7aa767388ebf605aa691e
itzRoma <romabondarenko03@gmail.com>
2021-10-15 20:25:18 +0300

    Some refactoring

--- src/main/java/com/lab111/TestMain.java
+++ src/main/java/com/lab111/TestMain.java
@@ -2,26 +2,23 @@ package com.lab111;
 
 /**
  * Template first class.
- * @author Alex
  *
+ * @author Alex
  */
 public final class TestMain {
+    /**
+     * Constructor.
+     */
+    private TestMain() {
+        super();
+    }
 
-  /**
-   * Constructor.
-   *
-   */
-  private TestMain() {
-    super();
-  }
-  
-  /**
-   * Invokes at application startup.
-   * @param args Parameters from command line
-   */
-  public static void main(final String[] args) {
-    System.out.println("This is template example for main class");
-
-  }
-
+    /**
+     * Invokes at application startup.
+     *
+     * @param args Parameters from command line
+     */
+    public static void main(final String[] args) {
+        System.out.println("This is template example for main class");
+    }
 }
